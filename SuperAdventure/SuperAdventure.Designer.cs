f316a3c615cfacecc0c5bd0198b815ffe309bbc8
Clive Strong <clive.strong@gmail.com>
2021-04-17 16:41:32 +0100

    Lesson 16.2
    Refactoring the player movement function

--- SuperAdventure/SuperAdventure.Designer.cs
+++ SuperAdventure/SuperAdventure.Designer.cs
@@ -154,7 +154,6 @@
             this.btnUseWeapon.TabIndex = 11;
             this.btnUseWeapon.Text = "Use";
             this.btnUseWeapon.UseVisualStyleBackColor = true;
-            this.btnUseWeapon.Click += new System.EventHandler(this.btnUseWeapon_Click_1);
             // 
             // btnUsePotion
             // 
@@ -164,7 +163,6 @@
             this.btnUsePotion.TabIndex = 12;
             this.btnUsePotion.Text = "Use";
             this.btnUsePotion.UseVisualStyleBackColor = true;
-            this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click_1);
             // 
             // btnNorth
             // 
